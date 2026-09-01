63f44020bdcd9bfd54ff360659accbd9968a9a5f
Shawon <shawonh986@gmail.com>
2026-09-02 01:19:08 +0800

    variables

---
+++ variables & datatypes.js
@@ -0,0 +1,21 @@
+//Variables
+
+
+fullName = "Shawon Hossain";
+
+console.log(fullName);
+age = 26;
+
+console.log(age);
+
+address = "Dhaka, Bangladesh";
+
+console.log(address);
+
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+
+//Data Types
+
