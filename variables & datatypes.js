c8e3fd1f327cffb69ab59ca9916f69829aece34a
Shawon <shawonh986@gmail.com>
2026-09-02 23:56:19 +0800

    datatypes, premitive and non premitive

--- variables & datatypes.js
+++ variables & datatypes.js
@@ -35,6 +35,30 @@ console.log(age);
 console.log(isStudent);
 
 
+//Primitives and Non-Primitives
+
+//Primitive data types
+var name = "Shawon"; //String
+var age = 26; //Number
+var isStudent = true; //Boolean
+var x = null; //Null
+var y = undefined; //Undefined
+var z = Symbol("id"); //Symbol
+var w = 10n; //BigInt
+
+//Non-Primitive data types
+var hobbies = ["Reading", "Writing", "Coding"]; //Array
+var address = { //Object
+    street: "123 Main St",
+    city: "Dhaka",
+    country: "Bangladesh",
+    postcode: "12345"
+};
+var greet = function() { //Function
+    console.log("Hello!");
+}
+greet();
+
 //var,let,const
 var x = 5; //var can be redeclared and updated
 let y = 10; //let can be updated but not redeclared
