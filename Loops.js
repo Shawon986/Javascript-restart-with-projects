5343c108d62ba3ddfaa3c2928a69659f888d642a
Shawon <shawonh986@gmail.com>
2026-09-04 03:26:45 +0800

    while loop

--- Loops.js
+++ Loops.js
@@ -18,3 +18,31 @@ for(let i = 0; i <= n; i++){
     sum += i;
 }
 console.log("The sum of first " + n + " numbers is " + sum);
+
+
+//while loop
+//print numbers from 0 to 4
+let i = 0;
+while(i < 5){
+    console.log("i = ", i);
+    i++;
+}
+
+//print odd numbers from 1 to 10
+
+let j = 1;
+while(j < 10){
+    console.log("j = ", j);
+    j += 2;
+}
+
+//print sum of k numbers
+
+let sums = 0;
+let k = 10;
+let m = 1;
+while(m <= k){
+    sums += m;
+    m++;
+}
+console.log("The sum of first " + k + " numbers is " + sums);
