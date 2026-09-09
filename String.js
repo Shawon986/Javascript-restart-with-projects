a0e1636e3544935cdb0d9cbe0d673fcef7a722cc
Shawon <shawonh986@gmail.com>
2026-09-09 22:51:52 +0800

    string method

---
+++ String.js
@@ -0,0 +1,8 @@
+//String methods
+let str = "Hello, World!";
+console.log(str.length); // 13
+console.log(str.charAt(0)); // "H"
+console.log(str.indexOf("World")); // 7
+console.log(str.slice(0, 5)); // "Hello"
+console.log(str.toUpperCase()); // "HELLO, WORLD!"
+console.log(str.toLowerCase()); // "hello, world!"
