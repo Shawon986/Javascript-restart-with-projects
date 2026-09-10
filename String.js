bd7ad236bde3c86667d8c4374a7462bfd0a7c1ec
Shawon <shawonh986@gmail.com>
2026-09-10 23:17:55 +0800

    template literals

--- String.js
+++ String.js
@@ -6,3 +6,15 @@ console.log(str.indexOf("World")); // 7
 console.log(str.slice(0, 5)); // "Hello"
 console.log(str.toUpperCase()); // "HELLO, WORLD!"
 console.log(str.toLowerCase()); // "hello, world!"
+
+
+//template literals
+let name = "Shawon";
+let age = 25;
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+let object = {
+    name: "Shawon",
+    age: 25
+};
+console.log(`My name is ${object.name} and I am ${object.age} years old.`);
