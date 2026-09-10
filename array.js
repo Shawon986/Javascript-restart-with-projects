c472e8b89f4db42105909d6ace5ec1223f5f9e44
Shawon <shawonh986@gmail.com>
2026-09-11 00:18:51 +0800

    printing method of an array

--- array.js
+++ array.js
@@ -5,3 +5,22 @@ console.log(fruits);
 console.log(fruits[0]); // "Apple"
 console.log(fruits[2]); // "Mango"
 
+
+//printing all elements of an array using for loop
+let numbers = [1, 2, 3, 4, 5];
+
+for(let index = 0; index < numbers.length; index++){
+    console.log("Index: " + index + ", Value: " + numbers[index]);
+}
+
+//printing all elements of an array using for-of loop
+for(let value of numbers){
+    console.log("Value: " + value);
+}   
+
+//printing all elements of an array using while loop
+let i = 0;
+while(i < numbers.length){
+    console.log("Index: " + i + ", Value: " + numbers[i]);
+    i++;
+}
